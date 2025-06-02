efa647f39a5b40863755251f0b839fc248c32d5c
Elyas Bianchi <elyasb99@gmail.com>
2025-06-02 12:29:08 -0700

    work

--- src/main.zig
+++ src/main.zig
@@ -1,31 +1,6 @@
 const std = @import("std");
+const show = @import("show.zig");
 
-//     while (args.next()) |arg| {
-//         const option = std.meta.stringToEnum(Option, arg) orelse {
-//             std.debug.print("{s} is not a valid argument\n", .{arg});
-//             help(process_name);
-//         };
-//
-//         switch (option) {
-//             .@"--address" => {
-//                 address = args.next() orelse {
-//                     std.debug.print("--address provided with no argument\n", .{});
-//                     help(process_name);
-//                 };
-//             },
-//             .@"--port" => {
-//                 const port_s = args.next() orelse {
-//                     std.debug.print("--port provided with no argument\n", .{});
-//                     help(process_name);
-//                 };
-//                 port = std.fmt.parseInt(u16, port_s, 10) catch {
-//                     std.debug.print("--port argument is not a valid u16\n", .{});
-//                     help(process_name);
-//                 };
-//             },
-//             .@"--help" => help(process_name),
-//         }
-//     }
 pub fn main() !void {
     const Option = enum {
         show,
@@ -52,7 +27,9 @@ pub fn main() !void {
             return;
         };
         switch (option) {
-            .show => {},
+            .show => {
+                show.show_main();
+            },
             else => {
                 std.log.err("option \x1b[31m{s}\x1b[0m is not supported", .{@tagName(option)});
                 return;
---
+++ src/show.zig
@@ -0,0 +1,5 @@
+const std = @import("std");
+
+pub fn show_main() void {
+    std.debug.print("calling show_main\n", .{});
+}
