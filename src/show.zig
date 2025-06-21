f3b73b5fbca7ab85073db33af628a01967ad3f0b
Elyas Bianchi <114762087+Elyasb14@users.noreply.github.com>
2025-06-20 20:34:18 -0700

    stopping

--- src/show.zig
+++ src/show.zig
@@ -9,12 +9,35 @@ const c = @cImport({
     @cInclude("net/if.h");
 });
 
-fn get_netif() void {
+const WGConfig = struct {
+    path: []const u8,
+    contents: []const u8,
+    interface: IFInfo,
+
+    pub fn init(path: []const u8, contents: []const u8, interface: IFInfo) WGConfig {
+        return .{ .path = path, .contents = contents, .interface = interface };
+    }
+};
+
+const IFInfo = struct {
+    name: []const u8,
+    addr: std.net.Ip4Address,
+
+    pub fn init(name: []const u8, addr: std.net.Ip4Address) IFInfo {
+        return .{ .name = name, .addr = addr };
+    }
+};
+
+fn get_netif(alloc: std.mem.Allocator) !?[]IFInfo {
+    var interfaces = std.ArrayList(IFInfo).init(alloc);
+    defer interfaces.deinit();
+
     var ifap: ?*c.struct_ifaddrs = null;
     if (c.getifaddrs(&ifap) != 0) {
         std.debug.print("getifaddrs failed\n", .{});
-        return;
+        return error.GetIFAddrsFailure;
     }
+    defer c.freeifaddrs(ifap);
 
     var ifa = ifap;
     while (ifa) |ifa_ptr| {
@@ -25,36 +48,34 @@ fn get_netif() void {
 
         const family = ifa_ptr.ifa_addr.*.sa_family;
         const name = std.mem.sliceTo(ifa_ptr.ifa_name, 0);
-        if (std.mem.startsWith(u8, name, "utun")) {
-            std.debug.print("Interface: {s}\n", .{name});
+        if (!std.mem.startsWith(u8, name, "utun")) {
+            ifa = ifa_ptr.ifa_next;
+            continue;
         }
 
         if (family == c.AF_INET) {
             const addr_in: *const c.struct_sockaddr_in = @alignCast(@ptrCast(ifa_ptr.ifa_addr));
             var buf: [c.INET_ADDRSTRLEN]u8 = undefined;
             const ip = c.inet_ntop(c.AF_INET, &addr_in.sin_addr, &buf, buf.len);
-            if (ip != null)
-                std.debug.print("IPv4: {s}\n", .{std.mem.sliceTo(ip, 0)});
+            if (ip == null) {
+                ifa = ifa_ptr.ifa_next;
+                continue;
+            }
+
+            const raw = std.mem.toBytes(@byteSwap(addr_in.sin_addr.s_addr));
+            const ip4_addr = std.net.Ip4Address.init(raw, 0);
+            const if_info = IFInfo.init(name, ip4_addr);
+            try interfaces.append(if_info);
         }
         ifa = ifa_ptr.ifa_next;
     }
-
-    c.freeifaddrs(ifap);
+    return try interfaces.toOwnedSlice();
 }
 
-const WGIFInfo = struct {
-    path: []const u8,
-    contents: []const u8,
-
-    pub fn init(path: []const u8, contents: []const u8) WGIFInfo {
-        return .{ .path = path, .contents = contents };
-    }
-};
-
-fn get_wg_configs(alloc: std.mem.Allocator) ![]WGIFInfo {
+fn get_wg_configs(alloc: std.mem.Allocator) ![]WGConfig {
     const tag = builtin.target.os.tag;
 
-    var files = std.ArrayList(WGIFInfo).init(alloc);
+    var files = std.ArrayList(WGConfig).init(alloc);
     defer files.deinit();
 
     const path = switch (tag) {
@@ -71,7 +92,7 @@ fn get_wg_configs(alloc: std.mem.Allocator) ![]WGIFInfo {
             const full_path = try std.fs.path.join(alloc, &[_][]const u8{ path, entry.name });
 
             const contents = try dir.readFileAlloc(alloc, entry.name, std.math.maxInt(usize));
-            const wfif = WGIFInfo.init(full_path, contents);
+            const wfif = WGConfig.init(full_path, contents);
             try files.append(wfif);
         }
     }
@@ -83,7 +104,11 @@ pub fn show_main(alloc: std.mem.Allocator) void {
         std.debug.print("ERROR: {any}\n", .{err});
         return;
     };
-    get_netif();
+    const netif = get_netif(alloc) catch return orelse return;
+
+    for (netif) |interface| {
+        std.debug.print("interface: {s}\n", .{interface.name});
+    }
 
     for (configs) |config| {
         std.debug.print("path: {s}\n", .{config.path});
